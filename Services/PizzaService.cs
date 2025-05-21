91b75b6d94de7bad2bd03c8a2da2cbf6ab325db1
danish ahmed <dan.ahmed@live.com>
2025-05-21 05:33:57 -0700

    added new pizza type

--- Services/PizzaService.cs
+++ Services/PizzaService.cs
@@ -11,7 +11,7 @@ public static class PizzaService
         Pizzas = new List<Pizza>
                 {
                     new Pizza { Id = 1, Name = "Classic Italian", Price=20.00M, Size=PizzaSize.    Large, IsGlutenFree = false },
-                    new Pizza { Id = 2, Name = "Veggie", Price=15.00M, Size=PizzaSize.Small,     IsGlutenFree = true }
+                    new Pizza { Id = 2, Name = "Veggie", Price=15.00M, Size=PizzaSize.Small,     IsGlutenFree = true },
                     new Pizza { Id = 2, Name = "New pizze", Price=46.00M, Size=PizzaSize.Small,     IsGlutenFree = true }
                 };
     }
