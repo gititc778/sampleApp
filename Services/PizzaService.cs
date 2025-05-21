23161c12cb2c9e23c0f8b50fc01dc84e28c4eecf
danish ahmed <dan.ahmed@live.com>
2025-05-21 05:49:12 -0700

    updated pizza

--- Services/PizzaService.cs
+++ Services/PizzaService.cs
@@ -12,7 +12,8 @@ public static class PizzaService
                 {
                     new Pizza { Id = 1, Name = "Classic Italian", Price=20.00M, Size=PizzaSize.    Large, IsGlutenFree = false },
                     new Pizza { Id = 2, Name = "Veggie", Price=15.00M, Size=PizzaSize.Small,     IsGlutenFree = true },
-                    new Pizza { Id = 2, Name = "New pizza", Price=46.00M, Size=PizzaSize.Small,     IsGlutenFree = true }
+                    new Pizza { Id = 2, Name = "New pizza", Price=46.00M, Size=PizzaSize.Small,     IsGlutenFree = true },
+                    new Pizza { Id = 2, Name = "vel", Price=46.00M, Size=PizzaSize.Small,     IsGlutenFree = true }
                 };
     }
     public static List<Pizza> GetAll() => Pizzas;
